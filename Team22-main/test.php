b44ff9b4204100af8c36cb986577b42b731bae0a
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-05-11 00:33:47 +0100

    Add files via upload

---
+++ Team22-main/test.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+$host = 'localhost';
+$dbname = 'part3DB';
+$username = 'root';
+$password = 'root';
+
+try {
+    // Create a PDO instance
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
