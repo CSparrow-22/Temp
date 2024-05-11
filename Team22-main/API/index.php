fb1cb411c7a3b4be24d3dee66643392ac2bfb1ac
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-05-11 15:32:52 +0100

    Update index.php

--- Team22-main/API/index.php
+++ Team22-main/API/index.php
@@ -14,7 +14,7 @@ $request_uri = rtrim($request_uri, '/'); // Remove trailing slash
 
 // Split the path into segments
 $path_segments = explode('/', $request_uri);
-$endpoint = $path_segments[1];
+$endpoint = $path_segments[4];
 
 
 // Check if the user is authenticated and authorized
