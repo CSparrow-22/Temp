e6d8f3fc0125fed1a845afab0424fbe7d90817e9
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-05-12 23:44:40 +0100

    Update data.php

--- Team22-main/DataAnalytics/data.php
+++ Team22-main/DataAnalytics/data.php
@@ -316,7 +316,23 @@
 
 
 <script>
+let url = 'http://34.142.47.100/Temp/Team22-main/API/index.php/activeemployees';
 
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+        console.log(data);
+        
+        
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
 
 </script>
 
